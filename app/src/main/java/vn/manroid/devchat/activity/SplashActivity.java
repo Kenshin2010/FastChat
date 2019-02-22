0593d0d8401429a4c03c951286542a69dd3a5aba
Manroid38 <phamdoanmanu38@gmail.com>
2019-02-22 21:39:59 +0700

    fix bug

--- app/src/main/java/vn/manroid/devchat/activity/SplashActivity.java
+++ app/src/main/java/vn/manroid/devchat/activity/SplashActivity.java
@@ -1,12 +1,20 @@
 package vn.manroid.devchat.activity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import vn.manroid.devchat.R;
 
@@ -25,6 +33,8 @@ public class SplashActivity extends AppCompatActivity {
         txtFont.setTypeface(Typeface.createFromAsset(getAssets(),
                 "myfont.ttf"));
 
+        getKeyHash();
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -34,5 +44,22 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         },4000);
+    }
+
+    private void getKeyHash(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "vn.manroid.devchat",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 }
